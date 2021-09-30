2c6f6d411f0714f3062bf1f017f3789b6bdaef25
thepurplesnail <vanessaman446@gmail.com>
2021-09-29 21:18:03 -0400

    Lab 8

---
+++ Labs/Lab 8/src/sumColumns.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class sumColumns {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the dimensions of an n x m matrix: ");
+        int rows = input.nextInt();
+        int columns = input.nextInt();
+        double[][] m = new double[rows][columns];
+        double result = 0;
+        System.out.println("Enter the matrix of "+ rows + " x " + columns + ": ");
+        for (int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                m[i][j] = input.nextDouble();
+            }
+        }
+        System.out.println("The sums of the columns are: ");
+        for (int c = 0; c < columns; c++){
+            result = sumColumns(m, c);
+            System.out.print(result + " ");
+        }
+
+    }
+    public static double sumColumns(double[][] m, int c){
+        double sum = 0;
+        for(int i = 0; i < m.length; i++) {
+            sum += m[i][c];
+        }
+        return sum;
+    }
+}
