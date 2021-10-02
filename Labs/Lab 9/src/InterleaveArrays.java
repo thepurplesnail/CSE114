ef83dc91392067cf9338ac22598d52641c1ffa52
thepurplesnail <vanessaman446@gmail.com>
2021-10-02 12:11:04 -0400

    Added more test cases to Interleave

--- Labs/Lab 9/src/InterleaveArrays.java
+++ Labs/Lab 9/src/InterleaveArrays.java
@@ -11,6 +11,13 @@ public class InterleaveArrays {
         int[] result3 = interleaveArrays(new int[]{1,3,5}, new int[]{2,4,6,8,10}); // returns [1, 2, 3, 4, 5, 6, 8, 10]
         System.out.println("New array is: " + Arrays.toString(result3));
 
+        int[] result4 = interleaveArrays(new int[]{1,3,5,7,9}, new int[]{2,4,6}); // returns [1, 2, 3, 4, 5, 6, 7, 9]
+        System.out.println("New array is: " + Arrays.toString(result4));
+
+        int[] result5 = interleaveArrays(new int[]{1}, new int[]{2,4,6,8,10}); // returns [1, 2, 4, 6, 8, 10]
+        System.out.println("New array is: " + Arrays.toString(result5));
+
+
     }
 
     public static int[] interleaveArrays(int[] arr1, int[] arr2){
