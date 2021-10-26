d11166a219bc2804ab7e5e447a41b38cc3341d84
thepurplesnail <vanessaman446@gmail.com>
2021-10-26 02:18:19 -0400

    Vowels, Consonants, Syllables

--- Labs/LAB 15/src/Syllables.java
+++ Labs/LAB 15/src/Syllables.java
@@ -17,32 +17,32 @@ public class Syllables {
             }
 
             totalSyl += numSylls;
-
         }
         System.out.print("Number of syllables entered: " + totalSyl);
 
     }
 
     public static int countSyl(String[] strArr){
-        String[] sylArr;
-        String vowel = "([aeiou]|[AEIOU])";
+        String vowel = "([aeiouAEIOU])";
         String consonant = "[^aeiouAEIOU]";
         int numVows = countVow(strArr);
         int count = 0;
         for (int i = 0; i < strArr.length; i++){
             if(i == 0 && strArr[i].matches(vowel)) //if it starts with a vowel
                 count++;
-            else if (i != 0 && strArr[i].matches("y")) // if it ends with y
+            else if (i != 0 && strArr[i].matches("[yY]")) // if it ends with y
                 count++;
-            else if (i == strArr.length - 1 && strArr[i].matches("a") && strArr[i - 1].matches("e")) // if last two letters ea
+            else if (i == strArr.length - 1 && strArr[i].matches("[aA]") && strArr[i - 1].matches("[eE]")) // if last two letters ea
                 count++;
-            else if (i == strArr.length - 1 && strArr[i].matches("e")){  // if it ends with e
+            else if (i == strArr.length - 1 && strArr[i].matches("[eE]")){  // if it ends with e
                 if(numVows == 1)  // if it ends with e and e is the only vowel
                     count++;
                 else
                     break; // otherwise e is silent
 
-            } else if (strArr[i].matches("a") && strArr[i - 1].matches("i"))  // if ia ever appears
+            } else if (strArr[i].matches("[aAoO]") && strArr[i - 1].matches("[iI]"))  // if ia or io ever appears
+                count++;
+            else if (strArr[i].matches("[oO]") && strArr[i - 1].matches("[eE]"))  // if eo ever appears
                 count++;
             else if (strArr[i].matches(vowel) && strArr[i - 1].matches(consonant))
                 count++;
@@ -69,29 +69,40 @@ public class Syllables {
     public static StringBuilder[] getSyl(int totalSyl, String[] strArr){
         StringBuilder[] sylArr = new StringBuilder[totalSyl];
         String vowel = "[aeiouAEIOU]";
+        String pseudoVowel = "[aeiouyAEIOUY]";
         String consonant = "[^aeiouAEIOU]";
         int pointer = 0;
         for(int i = 0; i < sylArr.length; i++){
             StringBuilder syl = new StringBuilder();
             for(int j = pointer; j < strArr.length; j++){
-                if(j == pointer && strArr[j].matches(vowel)){ // if syllable starts with vowel
+                if(strArr[j].matches(vowel)){ // if syllable starts with vowel
                 	syl.append(strArr[j]);
-                	if(j < strArr.length - 2 && (strArr[j+2].matches(vowel) || strArr[j+2].matches("y")) ) { // sharp vowel
+                	if(j < strArr.length - 2 && (strArr[j+2].matches(pseudoVowel)) ) { // sharp vowel
                         pointer++;
                         break;
+                    } else if (strArr[j].matches("[iIeE]") && strArr[j + 1].matches("[aoAO]")){
+                	    pointer++;
+                	    break;
+                    } else if (strArr[j].matches("[aoAO]") && strArr[j - 1].matches("[iIeE]")) {
+                	    pointer++;
+                        break;
                     } else {
-                        while (!strArr[j].matches(consonant) && j < strArr.length - 1) { // keep building syllable until reaches consonant
+                        while (!strArr[j].matches(consonant) && j < strArr.length - 1)  // keep building syllable until reaches consonant
                             syl.append(strArr[++j]);
-                        }
+
                         pointer = j + 1;
                         break;
                     }
                 } else {
                 	syl.append(strArr[j]);
-                    while(!strArr[j].matches(vowel) && j < strArr.length - 1) 
+                    while(!strArr[j].matches(pseudoVowel) && j < strArr.length - 1)
                         syl.append(strArr[++j]);
-                    while(!strArr[j].matches(consonant) && j < strArr.length - 1) 
-                    	syl.append(strArr[++j]);
+                    while(strArr[j].matches(pseudoVowel) && j < strArr.length - 1) {
+                        syl.append(strArr[++j]);
+                    }
+                    if(j == strArr.length - 2 && strArr[j + 1].matches("[eE]") && totalSyl == 1 && j < strArr.length - 1){ // add silent e to syllable
+                        syl.append(strArr[++j]);
+                    }
                     pointer = j + 1;
                     break;
                 }
