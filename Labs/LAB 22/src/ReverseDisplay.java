ec4e32ea7e6e0ea7e6aed43ec5fe03de4efceea6
thepurplesnail <vanessaman446@gmail.com>
2021-11-30 13:46:06 -0500

    Recursion I

--- Labs/LAB 22/src/ReverseDisplay.java
+++ Labs/LAB 22/src/ReverseDisplay.java
@@ -8,9 +8,7 @@ public class ReverseDisplay {
         reverseDisplay(n);
     }
     public static void reverseDisplay(int value){
-        if (value == 0)
-            System.out.print("");
-        else{
+        if (value > 0){
             System.out.print(value % 10);
             reverseDisplay(value/10);
         }
